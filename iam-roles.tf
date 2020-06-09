75eea4ed7b2a6bd474e84672d9c01d808a8e9862
Allan Denot <adenot@gmail.com>
2020-06-09 18:01:52 +1000

    Using a random string and max number of chars at the iam role and policy instead of name_prefix

--- iam-roles.tf
+++ iam-roles.tf
@@ -25,17 +25,24 @@ data "aws_iam_policy_document" "event_trust" {
   }
 }
 
+# Generate a random string to add it to the name of the Target Group
+resource "random_string" "iam_suffix" {
+  length      = 12
+  number      = true
+  min_numeric = 12
+}
+
 resource "aws_iam_role" "event" {
   count              = var.enable ? 1 : 0
-  name_prefix        = "rds-scheduler-${var.identifier}-"
+  name               = substr("rds-scheduler-${var.identifier}-${random_string.iam_suffix.result}", 0, 64)
   assume_role_policy = data.aws_iam_policy_document.event_trust[0].json
 }
 
 resource "aws_iam_role_policy" "event" {
-  count       = var.enable ? 1 : 0
-  name_prefix = "rds-scheduler-${var.identifier}-"
-  policy      = data.aws_iam_policy_document.event[0].json
-  role        = aws_iam_role.event[0].name
+  count  = var.enable ? 1 : 0
+  name   = substr("rds-scheduler-${var.identifier}-${random_string.iam_suffix.result}", 0, 64)
+  policy = data.aws_iam_policy_document.event[0].json
+  role   = aws_iam_role.event[0].name
 }
 
 
@@ -53,15 +60,15 @@ data "aws_iam_policy_document" "ssm_automation_trust" {
 
 resource "aws_iam_role" "ssm_automation" {
   count              = var.enable ? 1 : 0
-  name_prefix        = "rds-scheduler-ssm-${var.identifier}-"
+  name               = substr("rds-scheduler-ssm-${var.identifier}-${random_string.iam_suffix.result}", 0, 64)
   assume_role_policy = data.aws_iam_policy_document.ssm_automation_trust[0].json
 }
 
 resource "aws_iam_role_policy" "ssm_automation" {
-  count       = var.enable ? 1 : 0
-  name_prefix = "rds-scheduler-ssm-${var.identifier}-"
-  role        = aws_iam_role.ssm_automation[0].name
-  policy      = <<EOF
+  count  = var.enable ? 1 : 0
+  name   = substr("rds-scheduler-ssm-${var.identifier}-${random_string.iam_suffix.result}", 0, 64)
+  role   = aws_iam_role.ssm_automation[0].name
+  policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
